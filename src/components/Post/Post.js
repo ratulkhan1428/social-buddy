84c43d98f19e89fefb6e4f8cfbf3750ed53df1b1
Ratul <ratulkhan1428@gmail.com>
2021-03-08 11:11:19 +0600

    social-buddy

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const Post = (props) => {
     const {id, title, body} = props.post;
